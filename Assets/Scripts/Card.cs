cd191ea2366a128f0b94dc9eff99ab14b160f50c
Pranav Mahendra <ipranav92@hotmail.com>
2020-09-23 23:06:41 +0530

    Initial Commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card
+{
+    Card(string suit, int rank)
+    {
+        this.Suit = suit;
+        this.Rank = rank;
+    }
+
+    public string Suit { get; }
+    public int Rank { get; }
+}
---
+++ Assets/Scripts/CardDeck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDeck : MonoBehaviour
+{
+    private List<Card> deckOfCards;   // Contains 52 cards.
+
+    private void Start()
+    {
+        deckOfCards = new List<Card>();
+
+        for (int i = 0; i < 13; i++)
+        {
+            
+        }
+    }
+}
